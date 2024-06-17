b2525e9b2f824d1a0ce3358e957acf34dc5081ba
ananyaa7 <ananyaa7@illinois.edu>
2024-06-17 16:58:53 -0500

    initial code

--- main.py
+++ main.py
@@ -33,15 +33,30 @@
 
 #Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
 #11 is the Ace.
-#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+import random
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+def deal_card(cards):
+    return random.choice(cards)
 
 #Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-#user_cards = []
-#computer_cards = []
+user_cards = []
+computer_cards = []
+user_cards.append(deal_card(cards))
+user_cards.append(deal_card(cards))
+computer_cards.append(deal_card(cards))
+computer_cards.append(deal_card(cards))
 
 #Hint 6: Create a function called calculate_score() that takes a List of cards as input 
 #and returns the score. 
 #Look up the sum() function to help you do this.
+def calculate_score(tmplst):
+    tmplst.sort()
+    if tmplst[0]==11 and tmplst[1]==10:
+        #its a blackjack
+        return 0
+    
+
+    return sum(tmplst)
 
 #Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
 
